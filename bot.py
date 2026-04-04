eb25c356740aa1106b4ebc46a10f31b8193a36f2
Asdasd7117 <alywm100@gmail.com>
2026-04-04 23:25:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,8 +35,8 @@ from telegram.constants import ParseMode
 # ════════════════════════════════════════════════════════════
 #                    ⚙️  الإعدادات الأساسية
 # ════════════════════════════════════════════════════════════
-BOT_TOKEN       = "8796739637:AAE2KeCf4WOqvxUGl1tqSYUjnx1sXrhMbmI"
-OWNER_ID        = 7946243967
+BOT_TOKEN       = "8443614197:AAFF5awBt6UX3ZAcxsosWuDkVUUq8GOmuRg"
+OWNER_ID        = 6814152338
 DEPOSIT_ADDRESS = "TYourTRC20AddressHere"  # ⚠️ ضع عنوان محفظتك الحقيقي
 DB_PATH         = "trading_bot.db"
 
